0577618de8e307556da8478d67e7c13a56007ed7
Jamiu Abidemi Abdulkareem <106557387+jamiuabdulkareem@users.noreply.github.com>
2023-11-03 00:28:40 +0100

    update

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,11 +1,26 @@
-import { useState } from "react"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function SearchOrder() {
-   const [query, setQuery] = useState();
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
 
-   return (
-      <input placeholder="Search order number #" value={}/>
-   )
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!query) return;
+    navigate(`/order/${query}`);
+    setQuery("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Search order number #"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </form>
+  );
 }
 
-export default SearchOrder
+export default SearchOrder;
