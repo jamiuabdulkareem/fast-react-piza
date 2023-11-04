1c99615f83ec27db4b5327fd56a9c02a536a7cec
Jamiu Abidemi Abdulkareem <106557387+jamiuabdulkareem@users.noreply.github.com>
2023-11-05 00:30:12 +0100

    update

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Jamiu</div>;
+  return <div className="hidden text-sm font-semibold">Jamiu</div>;
 }
 
 export default Username;
