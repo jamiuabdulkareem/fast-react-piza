75285215ec8ad7f12094497409565b59a09611aa
Jamiu Abidemi Abdulkareem <106557387+jamiuabdulkareem@users.noreply.github.com>
2023-11-05 02:02:19 +0100

    update

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -81,7 +81,10 @@ function CreateOrder() {
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
 
-          <button disabled={isSubmiting}>
+          <button
+            disabled={isSubmiting}
+            className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2"
+          >
             {isSubmiting ? "Placing order..." : "Order now"}
           </button>
         </div>
