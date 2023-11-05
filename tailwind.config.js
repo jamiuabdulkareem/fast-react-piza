e665106e9e3da4c93bc2da830afa4502e1549b36
Jamiu Abidemi Abdulkareem <106557387+jamiuabdulkareem@users.noreply.github.com>
2023-11-05 22:57:44 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: "Roboto Mono, monospace",
+    },
+
     extend: {},
   },
   plugins: [],
