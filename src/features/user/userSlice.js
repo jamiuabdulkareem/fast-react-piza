ca512d2616915706a5e747e98e8b27379e7fcc74
Jamiu Abidemi Abdulkareem <106557387+jamiuabdulkareem@users.noreply.github.com>
2023-11-06 02:25:45 +0100

    Update userSlice.js

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,4 +1,5 @@
-import getAddress from "../../services/apiGeocoding"
+/*
+import getAddress from "../../services/apiGeocoding";
 
 function getPosition() {
   return new Promise(function (resolve, reject) {
@@ -21,3 +22,24 @@ async function fetchAddress() {
   // 3) Then we return an object with the data that we are interested in
   return { position, address };
 }
+*/
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  username: "",
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    updateName(state, action) {
+      state.username = action.payload;
+    },
+  },
+});
+
+export const { updateName } = userSlice.actions;
+
+export default userSlice.reducer;
