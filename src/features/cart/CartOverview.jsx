16e50895f6cb6fceb50812608fe12a715adc8847
Jamiu Abidemi Abdulkareem <106557387+jamiuabdulkareem@users.noreply.github.com>
2023-11-07 01:40:28 +0100

    update

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -7,6 +7,8 @@ function CartOverview() {
   const totalCartQuantity = useSelector(getTotalCartQuantity);
   const totalCartPrice = useSelector(getTotalCartPrice);
 
+  if (!totalCartQuantity) return null;
+
   return (
     <div className="flex  items-center justify-between bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
       <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
